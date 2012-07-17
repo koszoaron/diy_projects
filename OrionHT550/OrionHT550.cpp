ae639e9ceef381063e19c262471fc1a0896dab33
aronk <koszoaron@gmail.com>
2012-07-17 11:57:49 +0200

    Added project OrionHT550

---
+++ OrionHT550/OrionHT550.cpp
@@ -0,0 +1,68 @@
+// Do not remove the include below
+#include "OrionHT550.h"
+
+#define ENC_A 2
+#define ENC_B 3
+#define LED_CLK 5
+#define LED_DATA 6
+#define IR 7
+#define LED_EN1 8
+#define LED_EN2 9
+#define BTN_COM 11
+
+const byte numbers[10] = { B01000001, B11100111, B01010010, B01100010, B11100100, B01101000, B01001000, B11100011, B01000000, B01100000 };
+uint8_t value = 50;
+long encPosition = -999;
+
+Encoder encMain(ENC_A, ENC_B);
+
+//The setup function is called once at startup of the sketch
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(LED_CLK, OUTPUT);
+    pinMode(LED_DATA, OUTPUT);
+    pinMode(LED_EN1, OUTPUT);
+    pinMode(LED_EN2, OUTPUT);
+}
+
+void lightSegments(byte num) {
+    shiftOut(LED_DATA, LED_CLK, LSBFIRST, numbers[num]);
+}
+
+void lightTens(byte num) {
+    digitalWrite(LED_EN2, LOW);
+    digitalWrite(LED_EN1, HIGH);
+    lightSegments(num);
+}
+
+void lightOnes(byte num) {
+    digitalWrite(LED_EN1, LOW);
+    digitalWrite(LED_EN2, HIGH);
+    lightSegments(num);
+}
+
+void displayNumber(uint8_t num) {
+    uint8_t tens = num / 10;
+    uint8_t ones = num % 10;
+    lightTens(tens);
+    delay(10);
+    lightOnes(ones);
+    delay(10);
+}
+
+// The loop function is called in an endless loop
+void loop() {
+    long encNew = encMain.read();
+    if (encNew != encPosition) {
+        if (encNew > encPosition && encNew%4 != 0) {
+            value++;
+        } else if (encNew < encPosition && encNew%4 != 0) {
+            value--;
+        }
+
+        encPosition = encNew;
+    }
+
+    displayNumber(value);
+}
---
+++ OrionHT550/OrionHT550.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef OrionHT550_H_
+#define OrionHT550_H_
+#include "Arduino.h"
+//add your includes for the project OrionHT550 here
+#include "Encoder.h"
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project OrionHT550 here
+
+
+
+
+//Do not add code below this line
+#endif /* OrionHT550_H_ */
