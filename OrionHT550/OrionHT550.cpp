e0a77d7d9581d4cbe1a176bb4b436ec03e3fb29b
aronk <koszoaron@gmail.com>
2012-08-06 17:49:34 +0200

    Replaced main files: optimized pt2*** methods, constants moved to the header file. This version was tested with the amp and works (init, input select and volume control)

--- OrionHT550/OrionHT550.cpp
+++ OrionHT550/OrionHT550.cpp
@@ -1,30 +1,17 @@
 // Do not remove the include below
 #include "OrionHT550.h"
 
-#define ENC_A 2
-#define ENC_B 3
-#define LED_CLK 5
-#define LED_DATA 6
-#define IR 7
-#define LED_EN1 8
-#define LED_EN2 9
-#define BTN_COM 11
-#define IR_VOLDOWN 0x1FE50AF
-#define IR_VOLUP 0x1FEF807
-#define IR_REPEAT 0xFFFFFFFF
-
-const byte numbers[10] = { B01000001, B11100111, B01010010, B01100010, B11100100, B01101000, B01001000, B11100011, B01000000, B01100000 };
-uint8_t value = 50;
-long encPosition = -999;
+const byte numbers[10] = { SSEG_0, SSEG_1, SSEG_2, SSEG_3, SSEG_4, SSEG_5, SSEG_6, SSEG_7, SSEG_8, SSEG_9 };
+byte globalVolume = INITIAL_GLOBAL_VOLUME;
+long encPosition = INITIAL_ENCODER_POS;
 
 Encoder encMain(ENC_A, ENC_B);
-
 IRrecv irReceiver(IR);
 decode_results results;
 
 //The setup function is called once at startup of the sketch
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(SERIAL_SPEED);
     irReceiver.enableIRIn();
     Wire.begin();
 
@@ -32,41 +19,52 @@ void setup() {
     pinMode(LED_DATA, OUTPUT);
     pinMode(LED_EN1, OUTPUT);
     pinMode(LED_EN2, OUTPUT);
+    pinMode(MUTE_NEG, OUTPUT);
+
+    displayChar();
+    delay(1000);
+    initAmp();
 }
 
-void lightSegments(byte num) {
-    shiftOut(LED_DATA, LED_CLK, LSBFIRST, numbers[num]);
+void initAmp() {
+    setGlobalVolume(MAX_ATTENUATION);  //set the volume to the lowest setting
+    setMute(true);  //enable muting
+
+
+    delay(2000);  //wait 2 secs for the amp to settle
+    setInput(INPUT_STEREO);  //enable the stereo input
+    setSurroundEnhancement(false);
+    setMixerChannel6Db(false);
+    setMute(false);  //disable muting
+    globalVolume = 39;
+    setGlobalVolume(globalVolume);  //set the volume to the half of the max setting
 }
 
-void lightTens(byte num) {
-    digitalWrite(LED_EN2, LOW);
+void displayChar() {
     digitalWrite(LED_EN1, HIGH);
-    lightSegments(num);
-}
-
-void lightOnes(byte num) {
-    digitalWrite(LED_EN1, LOW);
     digitalWrite(LED_EN2, HIGH);
-    lightSegments(num);
+    shiftOut(LED_DATA, LED_CLK, LSBFIRST, SSEG_DASH);
 }
 
 void displayNumber(uint8_t num) {
-    uint8_t tens = num / 10;
-    uint8_t ones = num % 10;
-    lightTens(tens);
+    digitalWrite(LED_EN2, LOW);
+    digitalWrite(LED_EN1, HIGH);
+    shiftOut(LED_DATA, LED_CLK, LSBFIRST, numbers[num / 10]);
     delay(10);
-    lightOnes(ones);
+    digitalWrite(LED_EN1, LOW);
+    digitalWrite(LED_EN2, HIGH);
+    shiftOut(LED_DATA, LED_CLK, LSBFIRST, numbers[num % 10]);
     delay(10);
 }
 
-// The loop function is called in an endless loop
+// The loop function is called in an endvoid initAmp()less loop
 void loop() {
     long encNew = encMain.read();
     if (encNew != encPosition) {
         if (encNew > encPosition && encNew % 4 != 0) {
-            value++;
+            increaseVolume();
         } else if (encNew < encPosition && encNew % 4 != 0) {
-            value--;
+            decreaseVolume();
         }
 
         encPosition = encNew;
@@ -79,15 +77,79 @@ void loop() {
         Serial.println(results.value, HEX);
 
         if (results.value == IR_VOLDOWN) {
-            value--;
+            decreaseVolume();
         } else if (results.value == IR_VOLUP) {
-            value++;
+            increaseVolume();
         }
 
         irReceiver.resume();
     }
 
-    displayNumber(value);
+    displayNumber(globalVolume);
+}
+
+void setInput(byte input) {
+    switch (input) {
+        case INPUT_STEREO:
+            pt2323(PT2323_INST1);
+            break;
+        case INPUT_SURROUND:
+            pt2323(PT2323_IN6CH);
+            break;
+    }
+}
+
+void setSurroundEnhancement(bool enhancement) {
+    if (enhancement) {
+        pt2323(PT2323_SURRENH_ON);
+    } else {
+        pt2323(PT2323_SURRENH_OFF);
+    }
+}
+
+void setMixerChannel6Db(bool mix6db) {
+    if (mix6db) {
+        pt2323(PT2323_MIXCHAN_6DB);
+    } else {
+        pt2323(PT2323_MIXCHAN_0DB);
+    }
+}
+
+void setMute(bool mute) {
+    if (mute) {
+        pt2258(CHAN_MUTE, PT2258_ALLCH_MUTE);
+        pt2323(PT2323_ALL_MUTE);
+        digitalWrite(MUTE_NEG, LOW);
+    } else {
+        pt2258(CHAN_MUTE, PT2258_ALLCH_UNMUTE);
+        pt2323(PT2323_ALL_UNMUTE);
+        digitalWrite(MUTE_NEG, HIGH);
+    }
+}
+
+void increaseVolume() {
+    if (globalVolume < MAX_ATTENUATION) {
+        globalVolume++;
+        setGlobalVolume(globalVolume);
+    }
+}
+
+void decreaseVolume() {
+    if (globalVolume > MIN_ATTENUATION) {
+        globalVolume--;
+        setGlobalVolume(globalVolume);
+    }
+}
+
+void setGlobalVolume(byte volume) {
+    setChannelVolume(CHAN_ALL, volume);
+}
+
+void setChannelVolume(byte channel, byte volume) {
+    if (volume >= MIN_ATTENUATION && volume <= MAX_ATTENUATION) {
+        byte attenuation = MAX_ATTENUATION - volume;
+        pt2258(CHAN_ALL, attenuation);
+    }
 }
 
 void pt2323(byte command) {
@@ -96,73 +158,52 @@ void pt2323(byte command) {
     Wire.endTransmission();
 }
 
-//------------------------------------------------------------------------------
-// Volume controller IC command set
-//------------------------------------------------------------------------------
-void pt2258(byte command, byte ch)  // send volume level commands
-{
-    byte x10;
-    byte x1;
+void pt2258(byte channel, byte value) {
+    byte x10 = value / 10;
+    byte x1 = value % 10;
 
-    if (command >= 10) {
-        x10 = command / 10;        // set decade step
-        x1 = command % 10;       // set step
-    } else                       // set step
-    {
-        x1 = command;
-        x10 = 0;
-    }
-
-    switch (ch)                 // which channel to command
-    {
-        case 0:    // all channels
-            x1 = x1 + 0xe0;
-            x10 = x10 + 0xd0;
+    switch (channel) {
+        case CHAN_ALL:
+            x1 += PT2258_ALLCH_1DB;
+            x10 += PT2258_ALLCH_10DB;
             break;
-
-        case 1:    // channel 1
-            x1 = x1 + 0x90;
-            x10 = x10 + 0x80;
+        case CHAN_FL:
+            x1 += PT2258_FL_1DB;
+            x10 += PT2258_FL_10DB;
             break;
-
-        case 2:    // channel 2
-            x1 = x1 + 0x50;
-            x10 = x10 + 0x40;
+        case CHAN_FR:
+            x1 += PT2258_FR_1DB;
+            x10 += PT2258_FR_10DB;
             break;
-
-        case 3:    // channel 3
-            x1 = x1 + 0x10;
-            x10 = x10 + 0x00;
+        case CHAN_CEN:
+            x1 += PT2258_CEN_1DB;
+            x10 += PT2258_CEN_10DB;
             break;
-
-        case 4:    // channel 4
-            x1 = x1 + 0x30;
-            x10 = x10 + 0x20;
+        case CHAN_SW:
+            x1 += PT2258_SW_1DB;
+            x10 += PT2258_SW_10DB;
             break;
-
-        case 5:    // channel 5
-            x1 = x1 + 0x70;
-            x10 = x10 + 0x60;
+        case CHAN_RL:
+            x1 += PT2258_RL_1DB;
+            x10 += PT2258_RL_10DB;
             break;
-
-        case 6:    // channel 6
-            x1 = x1 + 0xb0;
-            x10 = x10 + 0xa0;
+        case CHAN_RR:
+            x1 += PT2258_RR_1DB;
+            x10 += PT2258_RL_10DB;
             break;
-
-        default:   // mute functions
-            if (command == 0)
-                x10, x1 = 0xf8;   // mute off
-            else
-                x10, x1 = 0xf9;               // mute on
+        case CHAN_MUTE:
+            if (value == 0) {
+                x10 = x1 = PT2258_ALLCH_MUTE;
+            } else {
+                x10 = x1 = PT2258_ALLCH_UNMUTE;
+            }
             break;
     }
 
-    for (int i = 0; i <= 2; i++)  // repeat 2x (had some unknown issues when transmitted only once)
-            {
-        Wire.beginTransmission(68); // transmit to device 0x88(hex) -> 136(dec)(addressing is 7-bit) -> 136/2
-        Wire.write(x10);             // send decade step
-        Wire.write(x1);              // send step
-        Wire.endTransmission();     // stop transmitting
+    for (int i = 0; i <= 2; i++) { // repeat 2x (had some unknown issues when transmitted only once)
+        Wire.beginTransmission(PT2258_ADDRESS);
+        Wire.write(x10);
+        Wire.write(x1);
+        Wire.endTransmission();
     }
 }
